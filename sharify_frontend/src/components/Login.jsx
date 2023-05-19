8ddd2d6ad0c165f62f52f365e604ef3e04aa0b09
AriaPzki <AriaPzki@gmail.com>
2023-05-19 11:07:01 -0400

    creating Login Page (Part 1)

--- sharify_frontend/src/components/Login.jsx
+++ sharify_frontend/src/components/Login.jsx
@@ -6,5 +6,20 @@ import ShareVideo from "../assets/share.mp4";
 import Logo from "../assets/logowhite.png";
 
 export default function Login() {
-  return <div>Login </div>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={ShareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay"></div>
+      </div>
+    </div>
+  );
 }
