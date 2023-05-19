21de46ff102dcbbe6700afb1b5cbc016f21bc741
AriaPzki <AriaPzki@gmail.com>
2023-05-19 10:41:45 -0400

    import assets

--- sharify_frontend/src/components/Login.jsx
+++ sharify_frontend/src/components/Login.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import ShareVideo from "../assets/share.mp4";
+import Logo from "../assets/logowhite.png";
 
 export default function Login() {
   return <div>Login </div>;
