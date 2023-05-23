9fe71054d47b334fb95bfa57391319eb9967bce9
AriaPzki <AriaPzki@gmail.com>
2023-05-23 19:08:17 -0400

    Start to connect to Sanity

--- sharify_frontend/src/components/Login.jsx
+++ sharify_frontend/src/components/Login.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import ShareVideo from "../assets/share.mp4";
 import Logo from "../assets/logowhite.png";
+import jwt_decode from "jwt-decode";
 
 export default function Login() {
+  const responseGoogle = (response) => {
+    const decoded = jwt_decode(response.credential); // We Used JSON Web Token to convert credential
+    // to details of the person logged in
+    console.log(decoded);
+    const user_decoded = [decoded.name, decoded.sub, decoded.picture];
+    const user = {
+      _id: user_decoded[1], // "_" is used to tell which document we are creating (user)
+      _type: "user",
+      username: user_decoded[0],
+      image: user_decoded[2],
+    };
+    console.log(user);
+  };
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -18,7 +32,33 @@ export default function Login() {
           autoPlay
           className="w-full h-full object-cover"
         />
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay"></div>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={Logo} alt="Logo" width="150px" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleOAuthProvider
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+            >
+              <GoogleLogin
+                render={(renderProps) => (
+                  <button
+                    type="button"
+                    className="bg-mainColor flex justify-center items-center p-3rounded-lg cursor-pointer outline-none"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FcGoogle className="mr-4" />
+                    Sign in with Google
+                  </button>
+                )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single-host-origin"
+              />
+            </GoogleOAuthProvider>
+          </div>
+        </div>
       </div>
     </div>
   );
