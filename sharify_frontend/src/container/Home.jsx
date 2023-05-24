a612fb2b8ccd19573b5ea772eed1ec2ae5ff5cea
AriaPzki <AriaPzki@gmail.com>
2023-05-24 11:13:05 -0400

    Components Created, Start Creating Home Page

--- sharify_frontend/src/container/Home.jsx
+++ sharify_frontend/src/container/Home.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import UserProfile from "../components/UserProfile";
+import { Sidebar, UserProfile } from "../components";
+import Client from "../client";
+import Logo from "../assets/logo.png";
+import Pins from "./Pins";
 
 export default function Home() {
   return <div>Home </div>;
