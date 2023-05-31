e847f0382ad0a327bfd5e136363ae0479387f90f
AriaPzki <AriaPzki@gmail.com>
2023-05-31 10:47:11 -0400

    Sidebar Developing-Part1

--- sharify_frontend/src/components/Sidebar.jsx
+++ sharify_frontend/src/components/Sidebar.jsx
@@ -1,5 +1,64 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
 
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+import logo from "../assets/logo.png";
+
+export default function Sidebar({ user, closeToggle }) {
+  const isNotActiveStyle =
+    "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+  const isActiveStyle =
+    "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
+
+  const categories = [
+    { name: "Animal" },
+    { name: "Wallpapers" },
+    { name: "Photography" },
+    { name: "Gaming" },
+    { name: "Coding" },
+  ];
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+  return (
+    <div className="flex flex-col justify-between bg-white h-full min-w-210  overflow-y-scroll hide-scrollbar ">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            Discover Categories
+          </h3>
+          {categories.slice(0, categories.length).map((category) => (
+            <NavLink
+              to={`/categories/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              key={category.name}
+              onClick={handleCloseSidebar}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
